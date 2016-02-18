b0214403c8e171e89d7c4b4386d4a7e64b2c6430
Farzin Ahmed <farzina@sfu.ca>
2016-02-17 16:50:42 -0800

    initial commit didnt carry changes over

--- prj2/main.c
+++ prj2/main.c
@@ -33,22 +33,43 @@ char history[HISTORY_DEPTH][COMMAND_LENGTH];
     int i=count;
     if (strcmp(buff,"") != 0){
        if (strlen(buff)!=0){
+          if(i>9)i=i-10;
           for (int j=0; j<strlen(buff); j++){
-             if(i>10)i=0;
              history[i][j] = buff[j];
-          }
+         }
        }
     }
  }
 
+/**
+ * TODO:
+ * The numbers are being printed correctly i.e the last 10 numbers
+ * are printed with correct numbering.
+ * Corresponding commands are wrong. Weird shit comes out.
+ * Im not sure if its print_history or add_history that does this.
+ * I'll keep working on it for now.
+ * @param count [description]
+ * @param buff  [description]
+ */
  void print_history(int count, char *buff){
 
-    for (int k=1; k<=count; k++){
-       printf("%d\t", k);
-       for (int l=0; l<=strlen(buff); l++){
-          printf ("%c", history[k-1][l]);
+    int k=0;
+    int n=1;
+    //TODO:Doesnt account for when count is greater than 20
+    if(count>10){
+      //k=count-10;
+      n=count-10;
+    }
+    while(n<=count){
+       printf("%d\t", n);
+       if (count>9) k=0;
+       for (int l=0; l<strlen(buff); l++){
+          printf ("%c", history[k][l]);
        }
        printf("\n");
+
+       k++;
+       n++;
     }
  }
 
@@ -67,6 +88,7 @@ int tokenize_command(char* buff, char* tokens[]){
    tokens[i] = NULL;
 
    return i;
+
 }
 
 void read_command(char *buff, char *tokens[], _Bool *in_background){
@@ -113,6 +135,8 @@ int main(int argc, char*argv[]){
    int result;
 
    //For Built-in commands
+   char *currentDir;
+   char *tmpDir;
    char* cwd;
    char blah[PATH_MAX + 1];
 
@@ -120,15 +144,74 @@ int main(int argc, char*argv[]){
       // Get command
       // Use write becausae we need to use read()/write() to work with
       // signals, and they are incompatible with print()
+      currentDir = getcwd(NULL, 0);
+      write(STDOUT_FILENO, currentDir, strlen(currentDir));
       write(STDOUT_FILENO, ">", strlen(">"));
       _Bool in_background = false;
       read_command(input_buffer, tokens, &in_background);
+
       if (strlen(input_buffer)!=0){
          add_history(input_buffer, count);
          count++;
       }
 
+      if(tokens[0] != NULL){
 
+   	 	if (strcmp(tokens[0], "exit") == 0){
+   	 	   //printf("the current is %s\n", tokens[0]);
+            return 0;
+         }
+         else if (strcmp(tokens[0], "pwd") == 0){
+            cwd = getcwd(blah, (PATH_MAX + 1));
+            printf("%s\n", cwd);
+            continue;
+         }
+         else if (strcmp(input_buffer, "history") == 0){
+            print_history(count, input_buffer);
+         }
+   		else if (strcmp(tokens[0], "cd") == 0){
+            currentDir = (getcwd(NULL, 0));
+            if (strchr(tokens[1], '/')){
+        			chdir(tokens[1]);
+        			printf( "Directory changed to %s\n", tokens[1]);
+     		    }
+        		else{
+        			//currentDir = (getcwd(NULL, 0));
+        			tmpDir = strcat(currentDir, "/");
+        			tokens[1] = strcat(tmpDir, tokens[1]);
+        			chdir(tokens[1]);
+        		}
+            continue;
+ 		   }
+    	// 	else if (strcmp(tokens[0], "cd") == -1){
+       //  		perror(tokens[1]);
+         // }
+
+         else {
+
+            pid_t child = fork();
+
+            if (child == 0){
+               if (execvp(tokens[0], tokens) < 0) {
+                  write(STDOUT_FILENO, "Error: exec failed\n", strlen("Error: exec failed\n"));
+                }
+               exit(1);
+            }
+
+            else if(child < 0){
+               write(STDOUT_FILENO, "Error: fork() failed\n", strlen("Error: fork() failed\n"));
+            }
+            else {
+               if (in_background == false){
+                  waitpid(child, &result, 0);
+               }
+            }
+            while (waitpid(-1, NULL, WNOHANG)>0){
+               ;
+            }
+
+         }
+      }
       /**
        * Steps for Basic Shell:
        * 1. Fork a child process
@@ -136,41 +219,7 @@ int main(int argc, char*argv[]){
        * 3. If in_background is false, parent waits for child to finish.
        * Otherwise, parent loops back to read_command() again immediately
        */
-      pid_t child = fork();
-      if (child == 0){
 
-         if(tokens[0] != NULL){
-            if (strcmp(tokens[0], "exit") == 0){
-               return 0;
-            }if (strcmp(tokens[0], "pwd") == 0){
-               cwd = getcwd(blah, (PATH_MAX + 1));
-               printf("%s\n", cwd);
-            }
-            if (strcmp(tokens[0], "history") == 0){
-               print_history(count, input_buffer);
-            }
-            else{
-               execvp(tokens[0], tokens);
-
-               write(STDOUT_FILENO, "Error: exec failed\n", strlen("Error: exec failed\n"));
-            }
-         }
-
-      }
-      else if(child < 0){
-         write(STDOUT_FILENO, "Error: fork() failed\n", strlen("Error: fork() failed\n"));
-      }
-      else {
-         if (in_background == false){
-            waitpid(child, &result, 0);
-         }
-         else {
-            continue;
-         }
-      }
-      while (waitpid(-1, NULL, WNOHANG)>0){
-         ;
-      }
    }
 
    return 0;
