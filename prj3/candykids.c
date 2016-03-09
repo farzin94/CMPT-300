728792f6f1fac991a508c7ab59ff3a9e59a289b3
Farzin Ahmed <farzina@sfu.ca>
2016-03-09 13:08:18 -0800

    files to begin prj3

---
+++ prj3/candykids.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+ // 1. Extract arguments
+ // 2. Initialize modules
+ // 3. Launch candy-factory threads
+ // 4. Launch kid threads
+ // 5. Wait for requested time
+ // 6. Stop candy-factory threads
+ // 7. Wait until no more candy
+ // 8. Stop kid threads
+ // 9. Print statistics
+ // 10. Cleanup any allocated memory
+}
