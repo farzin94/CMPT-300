fd2399aa4a24aa6ae1afdcf8eb26af066bd293bf
Farzin Ahmed <farzin94mimmi@gmail.com>
2016-03-17 16:29:24 -0700

    bbuff is usually empty it seems

--- prj3/candykids.c
+++ prj3/candykids.c
@@ -18,6 +18,7 @@
  */
 
 _Bool stop_thread = false;
+//pthread_mutex_t lock;
 
 
 typedef struct {
@@ -39,8 +40,9 @@ void *factoryFunc(void *p){
 	
 	int fac = *((int *) p);
 	srand(time(NULL));
-	while(stop_thread==false){
-		
+	
+	
+	while(!stop_thread){
 		int rand_sec = rand()%3;
 		
 		printf("\tFactory %d ships candy & waits %ds\n", fac, rand_sec );
@@ -48,21 +50,20 @@ void *factoryFunc(void *p){
 		candy_t* candy = malloc(sizeof(candy_t));
 		candy->factory_number = fac;
 		candy->time_stamp_in_ms = current_time_in_ms();
-		
-		//printf("time: %f\n", candy->time_stamp_in_ms);
+
 		
 		bbuff_blocking_insert(candy);
-		
-		
-		
+
 		sleep(rand_sec);	
 		free(candy);
+			
 	}
+	//
 	//assert(!bbuff_is_empty());
 	printf("Candy-factory %d done\n", fac);
 	//printf ("fac: %d\n", fac);
 	
-	//return candy;
+	//return NULL;
 }
 
 void *kidFunc(void *p){
@@ -73,29 +74,33 @@ void *kidFunc(void *p){
 		
 		//printf("*\n");
 		candy_t* candy = bbuff_blocking_extract();
-			//Process ?
-		printf("time: %f\n", candy->time_stamp_in_ms);
-		 
-		sleep(rand()%1);
 		
+		printf("fac num: %d\n", candy->factory_number);
+		printf("time: %f\n", candy->time_stamp_in_ms);   
+		sleep(rand()%1);
 	}
-	
-	
-	//return NULL;
+
+	return NULL;
 }
 
 int main(int argc, char *argv[]) {
 
+	//bbuff_init();
+	sem_init(&empty, 0, 0); // MAX buffers are empty to begin with...
+	sem_init(&full, 0, BUFFER_SIZE);    // ... and 0 are full
+	sem_init(&mutex, 0, 1);
+	
 	int factory=0;
 	int kid=0;
 	int sec=0;
 	
 	
 	
-	assert(bbuff_is_empty());
+	//assert(bbuff_is_empty());
 	//-----1. Extract arguments-----------------
 	if (argc == 0){
 		printf("Error: Invalid input\n");
+		exit(1);
 	}
 
 	if (argc > 0 && argc <= 4){
@@ -106,33 +111,27 @@ int main(int argc, char *argv[]) {
 	
 	if (factory <= 0 || kid <= 0 || sec <= 0){
 		printf("Error: Invalid input\n");
+		exit(1);
 	}
 	
 	printf("factory: %d, kids: %d, seconds: %d\n", factory , kid , sec);
 	
 	 //-----2. Initialize modules-------------------
+
 	
-	bbuff_init();
-	
-	/*candy_t* candy = malloc(sizeof(candy_t));
-		candy->factory_number = 0;
-		candy->time_stamp_in_ms = current_time_in_ms();
-		
-		//printf("time: %f\n", candy->time_stamp_in_ms);
-		
-		bbuff_blocking_insert(candy);*/
+	//------3. Launch candy-factory threads-----------
 	
 	pthread_t factory_thread[factory]; //to hold factory threads
 	int factory_num[factory]; //to pass factory number to factoryFunc
 	
-	for (int i=0; i<factory; i++){
-		factory_num[i] = i;
-		//printf("factory_num: %d\n", factory_num[i]);
+	for (int k=0; k<factory; k++){
+		factory_num[k] = k;
 	}
-		
-	//------3. Launch candy-factory threads-----------
+	
 	for (int i = 0; i < factory; i++) {
-		if(pthread_create(&factory_thread[i], NULL, factoryFunc, &factory_num[i])) {
+		pthread_attr_t attr;
+        pthread_attr_init(&attr);
+		if(pthread_create(&factory_thread[i],&attr, factoryFunc, &factory_num[i])) {
 			printf ("Create pthread error!\n");
 			exit (1);
 		}
@@ -142,7 +141,9 @@ int main(int argc, char *argv[]) {
 	
 	//------4. Launch kid threads-------------------
 	for (int i = 0; i < kid; i++) {
-		if(pthread_create(&kid_thread[i], NULL, kidFunc, NULL)) {
+		pthread_attr_t attr;
+        pthread_attr_init(&attr);
+		if(pthread_create(&kid_thread[i], &attr, kidFunc, NULL)) {
 			printf ("Create pthread error!\n");
 			exit (1);
 		}
@@ -155,18 +156,27 @@ int main(int argc, char *argv[]) {
 	}
 	
 	//------6. Stop candy-factory threads-----------
-	
+	stop_thread = true;
 	for (int x=0; x<factory; x++){
-		stop_thread = true;
-		pthread_join(factory_thread[x], NULL);
+		pthread_join(factory_thread[x],  NULL);
 	}
+	/*int i;
+	pthread_join(factory_thread[0],  (void **)&i);
+			printf("%d\n", i);
+	pthread_join(factory_thread[1],  (void **)&i);
+			printf("%d\n", i);
+	pthread_join(factory_thread[2],  (void **)&i);
+			printf("%d\n", i);*/
+		printf("hi\n");
 	
 	//------7. Wait until no more candy--------------
+	//assert(!bbuff_is_empty());
 	while (!bbuff_is_empty()){
-		printf("Waiting for all candy to be consumed");
+		printf("Waiting for all candy to be consumed\n");
 		sleep(1);
 	}
 	
+	printf("bye\n");
 	//------8. Stop kid threads----------------------
 	for (int i = 0; i < kid; i++) {
 		pthread_cancel(kid_thread[i]);
@@ -175,4 +185,7 @@ int main(int argc, char *argv[]) {
 
 	//------9. Print statistics-----------------
 	//------10. Cleanup any allocated memory---------
+	//pthread_mutex_destroy(&lock);
+	 
+	 return 0;
 }
