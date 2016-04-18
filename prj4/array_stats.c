ebc19ddcaf9874be048828a21114137afaabc17e
apaunescu <apaunescu@users.noreply.github.com>
2016-04-17 19:47:36 -0700

    Added files via upload

--- prj4/array_stats.c
+++ prj4/array_stats.c
@@ -1,5 +1,7 @@
 #include "array_stats.h"
-#include <errno.h>
+#include <linux/kernel.h>
+#include <linux/uaccess.h>
+//#include <errno.h>
 
 /**
  * [sys_array_stats Set the three fields of the stats structure based on the data array.
@@ -14,9 +16,52 @@
  * @param  size  [ The number of elements in data. Must be > 0 ]
  * @return [ 0 when successful, -EINVAL when size <=0, -EFAULT for mem access]
  * sys-call number = 331
+	struct array_stats{
+	long min;
+	long max;
+	long sum;
+};
  */
-asmlinkage 	long sys_array_stats(
-	struct array_stats *stats,
-	long data[],
-	long size
-);
+asmlinkage long sys_array_stats(struct array_stats *stats, long data[], long size)
+{
+	struct array_stats copied = {10000000, 0, 0};
+	long dataCopied = 0;
+	//long len = 0;
+	long result = 0;
+	int i = 0;
+	while (i<size)
+	{
+		/* copy src, which is in the user’s address space, into buf */
+		if (copy_from_user(&dataCopied, &data[i], sizeof(data[i])))
+		{
+			return -EFAULT;
+		}
+		/* copy buf into dst, which is in the user’s address space */ 		
+		if (copied.min > dataCopied)
+		{
+			copied.min = dataCopied;
+		}
+		printk("--min %ld\n", copied.min);
+		if (copied.max < dataCopied)
+		{
+			copied.max = dataCopied;
+		}
+		printk("--max %ld\n", copied.max);
+		
+		copied.sum = (copied.sum + dataCopied);
+		printk("--sum %ld\n", copied.sum);
+		
+		printk("--data %ld\n", dataCopied);
+		i++;
+	}
+	/* copy buf into dst, which is in the user’s address space */
+	if (copy_to_user(stats, &copied, sizeof(copied)))
+	{
+		return -EFAULT;
+	}
+	printk("--STATSmax %ld\n", stats->min);
+	printk("--STATSmax %ld\n", stats->max);
+	printk("--STATSmax %ld\n", stats->sum);
+	return result;
+}
+
